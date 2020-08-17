6dede7edd3d4664b55d499375f402a584586aba1
Manikant Jha <mjontop0602@gmail.com>
2020-08-17 15:38:47 +0530

    end=

--- python.py
+++ python.py
@@ -1,12 +1,11 @@
-print("hello World,")
-print("Welcome to the python class")
+# #  print("hello World,")
+# print("Welcome to the python class")
 
-print("Are you enjoying?")
-
-
+# print("Are you enjoying?")
 
 
-print("hello World,", end=" ")
-print("Welcome to the python class")
+print("hello World,", end="*")
+print("Welcome to the python class", end="*")
 
-print("Are you enjoying?")
+# print("Are you enjoying?")
+print("Are you enjoying? ", end="\n")

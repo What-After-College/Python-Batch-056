65762a70198ad5ad6d88813dbde0fc3675eeed49
Manikant Jha <mjontop0602@gmail.com>
2020-08-17 15:59:10 +0530

    data type

--- python.py
+++ python.py
@@ -4,8 +4,17 @@
 # print("Are you enjoying?")
 
 
-print("hello World,", end="*")
-print("Welcome to the python class", end="*")
+# print("hello World,", end="\t")
+# print("Welcome to the python class", end="\t")
 
 # print("Are you enjoying?")
-print("Are you enjoying? ", end="\n")
+# print("Are you enjoying? ", end="\n")
+
+
+name = 'ritik'
+
+age = 18
+# state = 'haryana'
+# print(name, age, state, sep="\t") 
+print(type(name))
+print(type(age))

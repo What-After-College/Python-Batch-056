3013db628f2c13c62bd7cf1a21eff265bbcb8f86
Manikant Jha <mjontop0602@gmail.com>
2020-08-17 17:13:27 +0530

    if else done

--- inputs.py
+++ inputs.py
@@ -16,3 +16,103 @@
 
 # print("Diffrence is: ", res)
 
+# num1 = int(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# product = num1*num2
+
+# print("The product is: ", product)
+
+# num1 = float(input("Enter a number: "))
+# num2 = float(input("Enter another number: "))
+
+# q = num1/num2
+
+# q2 = num1//num2 # integer division
+# print(q)
+# print(q2)
+
+
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# a = 1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+# print(a)
+# a = a +1
+
+
+
+
+
+# a = 15
+# a = 1323
+# print(a)
+
+
+# num1 = 2
+# num2 = 100
+# res = num1**num2
+# print(res)
+
+
+
+# num1 = 16
+# num2 = 10
+# res = num1%num2
+# print(res)
+
+
+
+# num1 =2 
+# num2 = 1000000000
+# res = num1**num2
+# ans = res%100000
+
+# print(ans)
+
+
+# num1 = 2
+# num2 = 1000000000
+# mod =100000
+# res = pow(num1, num2, mod)  # num1**num2%mod
+# print(res)
+
+
+# condition = 0
+
+# if condition:
+#     print("true")
+# else:
+#     print("false")
+
+
+num = int(input("Enter number to check: "))
+if num%2 == 0:
+    print("Even")
+else:
+    print("ODD")

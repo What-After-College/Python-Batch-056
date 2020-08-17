e26be47679b09c720f18ceb7b0f4706d87c6cb12
Manikant Jha <mjontop0602@gmail.com>
2020-08-17 15:34:22 +0530

    print done

---
+++ python.py
@@ -0,0 +1,12 @@
+print("hello World,")
+print("Welcome to the python class")
+
+print("Are you enjoying?")
+
+
+
+
+print("hello World,", end=" ")
+print("Welcome to the python class")
+
+print("Are you enjoying?")

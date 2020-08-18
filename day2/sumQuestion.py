7554072b9abbe423abda1755e7357057bd8c0704
Manikant Jha <mjontop0602@gmail.com>
2020-08-18 16:04:58 +0530

    while loop ended

--- day2/sumQuestion.py
+++ day2/sumQuestion.py
@@ -1,8 +1,24 @@
-n = int(input("Enter the Number: "))
+# n = int(input("Enter the Number: "))
 
-i= 1
-sum = 0
-while i<=n:
-    sum += i
-    i += 1
+# i= 1
+# sum = 0
+# while i<=n:
+#     sum += i
+#     i += 1
+# print(sum)
+
+# n = int(input("Enter the number: "))
+
+# i=1
+# sum =0
+# while i<=n:
+#     sum += (i*i)
+#     i += 1
+# print(sum)
+
+
+n = int(input("Enter a number: "))
+
+sum = int((n*(n+1)*((2*n)+1))/6)
+
 print(sum)

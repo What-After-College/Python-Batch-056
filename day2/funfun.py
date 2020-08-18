1f5885d0355f173252e592d0297cbf09c4c0e25b
Manikant Jha <mjontop0602@gmail.com>
2020-08-18 17:36:21 +0530

    odd even using funtion

--- day2/funfun.py
+++ day2/funfun.py
@@ -12,7 +12,17 @@
 # sayHello('nikhil')
 
 
-def square(num):
-    return num*num
+# def square(num):
+#     return num*num
 
-print(square(5))
+# print(square(5))
+
+def checkOdd(num):
+    if num%2 == 0:
+        print("EVEN")
+    else:
+        print("ODD")
+
+checkOdd(6)
+checkOdd(10)
+checkOdd(11)

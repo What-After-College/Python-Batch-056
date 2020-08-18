f8a0e3d6f0b73fe429b8cdd940330cb982a948db
Manikant Jha <mjontop0602@gmail.com>
2020-08-18 15:43:41 +0530

    sum of first n number que

---
+++ day2/loops01.py
@@ -0,0 +1,29 @@
+# print("apple")
+# print("apple")
+# print("apple")
+# print("apple")
+# print("apple")
+# print("apple")
+# print("apple2")
+# print("apple2")
+# print("apple2")
+# print("apple2")
+
+# i = 1
+# while i<=10:
+#     print(i)
+#     i = i+1
+
+# 1 2 3 4 5 6 7 8 9 10
+
+i = 1
+while i<=10:
+    print(i, end=" ") 
+    i += 1     #i = i+1 
+
+# input = 5 
+# print(1+2+3+4+5)
+
+# input = 10
+# print = 1+2+3+4+5+6+7+8+9+10
+

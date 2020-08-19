7a8d78be1967d817a3c29a2c984a60ee40518ae2
Manikant Jha <mjontop0602@gmail.com>
2020-08-19 16:17:12 +0530

    array intro

---
+++ day3/arr1.py
@@ -0,0 +1,19 @@
+# student1 = 40
+# student2 = 60
+# student3 = 30
+# student4 = 50
+# student5 = 55
+
+# print(student1)
+# print(student2)
+# print(student5)
+
+
+#           0    1   2  3   4
+students = [40, 60, 30, 50, 55]
+# print(students[1])
+
+i=0
+while i<5:
+    print(students[i])
+    i += 1

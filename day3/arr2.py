71f09aafe720495961c0c3fb0a38a3c86eb18b70
Manikant Jha <mjontop0602@gmail.com>
2020-08-19 17:15:00 +0530

    added 2D and slicing

---
+++ day3/arr2.py
@@ -0,0 +1,50 @@
+
+
+#           0       1           2           3
+#         -4        -3         -2          -1
+# cites = ['Delhi', 'Mumbai', 'Kolkata', 'Chennai']
+# # print(cites[2])
+
+# print(cites[-4])
+
+#                       0               1                   2                       3
+#                   0       1           0       1       0           1           0           1
+# cityCountry = [['Delhi', 'india'], ['London', 'UK'], ['ontario', 'Canada'], ['moscow', 'Russia']]
+
+# # print(type(cityCountry))
+# # print(type(cityCountry[1]))
+
+# print(cityCountry[1][0][])
+
+
+# states = ['Delhi', 'Haryana', 'UP', 'Rajsthan', 'West Bengal']
+
+# print(states)
+# states[2] = 'Kashmir'
+
+# print(states)
+
+# states = ['Delhi', 'Haryana', 'UP', 'Rajsthan', 'West Bengal']
+
+# print(states)
+# states.append('Kashmir')
+
+# print(states)
+
+# states = ['Delhi', 'Haryana', 'UP', 'Rajsthan', 'West Bengal']
+
+# print(states)
+
+# del(states[3])
+
+# print(states)
+
+states = ['Delhi', 'Haryana', 'UP', 'Rajsthan', 'West Bengal']
+
+print(states)
+
+
+
+print(states[1:4])
+
+# https://www.tutorialspoint.com/python3/python_lists.htm

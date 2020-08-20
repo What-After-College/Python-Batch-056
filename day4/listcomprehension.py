1af03af6592ea35229f8493049021b3f9475b4c0
Manikant Jha <mjontop0602@gmail.com>
2020-08-20 15:11:46 +0530

    list comprehension

---
+++ day4/listcomprehension.py
@@ -0,0 +1,18 @@
+# = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# arr = []
+# for i in range(1, 10):
+#     arr.append(i)
+#     print(arr)
+
+# print(arr)
+
+
+# List Comprehension
+
+arr = [x for x in range(1, 10)]
+print(arr)
+
+
+evens = [x for x in range(2, 21) if x%2==0]
+print(evens)

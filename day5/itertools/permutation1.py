5741337479f3b717d7914e1095145a67ced49341
Manikant Jha <mjontop0602@gmail.com>
2020-08-21 16:39:22 +0530

    module math done

---
+++ day5/itertools/permutation1.py
@@ -0,0 +1,10 @@
+# import math
+
+from math import floor
+
+num = -75.8
+# print(math.sqrt(num))
+# print(math.ceil(num))  # (big or equal) and nearest
+# print(math.floor(num)) # (small or equal) and nearest
+
+print(floor(num)) # (small or equal) and nearest

1e8218f959b2dba19bc4a40969558553ef915326
Manikant Jha <mjontop0602@gmail.com>
2020-08-21 15:11:18 +0530

    added whats your name solution

---
+++ day5/solutions-of-hackerrank/whatsurname.py
@@ -0,0 +1,7 @@
+
+
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+
+def print_full_name(a, b):
+    # print("Hello "+a +" "+b+"! You just delved into python.")
+    print("Hello {} {}! You just delved into python.".format(a, b))

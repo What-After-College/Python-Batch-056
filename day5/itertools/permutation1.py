2ca6763805b3bdfb02d988a03c76fd688e1ea3cb
Manikant Jha <mjontop0602@gmail.com>
2020-08-21 16:49:01 +0530

    permutations itertools done

--- day5/itertools/permutation1.py
+++ day5/itertools/permutation1.py
@@ -1,10 +1,23 @@
-# import math
+# # import math
 
-from math import floor
+# from math import floor
 
-num = -75.8
-# print(math.sqrt(num))
-# print(math.ceil(num))  # (big or equal) and nearest
-# print(math.floor(num)) # (small or equal) and nearest
+# num = -75.8
+# # print(math.sqrt(num))
+# # print(math.ceil(num))  # (big or equal) and nearest
+# # print(math.floor(num)) # (small or equal) and nearest
 
-print(floor(num)) # (small or equal) and nearest
+# print(floor(num)) # (small or equal) and nearest
+
+from itertools import permutations
+
+# pr = permutations([4,5,6])
+
+# for i in pr:
+#     print(i)
+
+
+pr = permutations([1, 2, 3, 5], 2)
+
+for i in pr:
+    print(i)

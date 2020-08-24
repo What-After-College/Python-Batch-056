1b41614399d92a1708e5858ace5efe57a72fe037
Manikant Jha <mjontop0602@gmail.com>
2020-08-24 14:13:25 +0530

    random function added

---
+++ day06/rndm1.py
@@ -0,0 +1,18 @@
+import random 
+
+# num = random.randint(10, 100) # 10 included 100 indeded
+# print(num)
+
+
+
+num2 = random.randrange(10, 100)  # 10 included 100 not included
+
+#  10 
+# 56
+#  72
+# 45
+# 100 
+# 6 
+# 115
+
+print(num2)

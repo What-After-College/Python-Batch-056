4727ee43c58ab11e6c9a461a742f14eb436a24ea
Manikant Jha <mjontop0602@gmail.com>
2020-08-25 14:23:32 +0530

    tuple added

---
+++ day07/numpy01.py
@@ -0,0 +1,12 @@
+arr = [45, 56, 23, 89]
+
+arr[2] = 500
+
+print(arr)
+
+# # fruits = ['apple', 'strawbery', 'watermelon', 'grapes']
+# fruits = ('apple', 'strawbery', 'watermelon', 'grapes')
+
+# fruits[1] = 'mango'
+
+# print(type(fruits))
